dde198e90b20ea84ea50001b9ae15b19d97ba0f5
Themi <Tcm4760@gmail.com>
2022-05-04 13:22:58 -0500

    updated ao3 to grab correct file

--- ao3.py
+++ ao3.py
@@ -3,7 +3,7 @@ import db
 
 
 def dl_work(work_id, updated_time):
-    response = requests.get("https://archiveofourown.org/downloads/38775681/file.pdf")
+    response = requests.get(f"https://archiveofourown.org/downloads/{work_id}/file.pdf")
     data = response.content
     db.save_work(work_id, updated_time, data)
     return data
