9c2270679e2c548db93763b0dc55016e4e103475
Themi <Tcm4760@gmail.com>
2022-05-10 23:28:49 -0500

    Added print statement to notify of when a download is being done

--- ao3.py
+++ ao3.py
@@ -3,6 +3,7 @@ import db
 
 
 def dl_work(work_id, updated_time):
+    print(f"downloading {work_id} updated at {updated_time}...")
     response = requests.get(f"https://archiveofourown.org/downloads/{work_id}/file.pdf")
     data = response.content
     db.save_work(work_id, updated_time, data)
