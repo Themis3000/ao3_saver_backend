30e7757792d30b77e0767fb212895f237f041faa
Themi <Tcm4760@gmail.com>
2022-05-11 21:46:38 -0500

    Now fetches from download.archiveofourown.org, skipping the redirect step

--- ao3.py
+++ ao3.py
@@ -10,7 +10,7 @@ if "PROXYADDRESS" in os.environ:
 
 def dl_work(work_id, updated_time):
     print(f"downloading {work_id} updated at {updated_time}...")
-    response = requests.get(f"https://archiveofourown.org/downloads/{work_id}/file.pdf", proxies=proxies)
+    response = requests.get(f"https://download.archiveofourown.org/downloads/{work_id}/file.pdf", proxies=proxies)
     if not response.ok or response.headers["Content-Type"] != "application/pdf":
         return False
     data = response.content
