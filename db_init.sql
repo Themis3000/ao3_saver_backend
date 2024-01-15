5a687b7406794f4e22017d8cf13750e51f0ca30e
Themi Megas <Tcm4760@gmail.com>
2024-01-15 12:28:00 -0600

    updated db init query

--- db_init.sql
+++ db_init.sql
@@ -1,4 +1,5 @@
 CREATE TABLE "queue"(
+    "job_id" SERIAL NOT NULL,
     "work_id" INTEGER NOT NULL,
     "updated" BIGINT NOT NULL,
     "submitted_by" VARCHAR(255) NOT NULL,
@@ -8,16 +9,18 @@ CREATE TABLE "queue"(
     "disbatched_time" BIGINT NULL
 );
 ALTER TABLE
-    "queue" ADD PRIMARY KEY("work_id");
+    "queue" ADD PRIMARY KEY("job_id");
+CREATE INDEX "queue_work_id_index" ON
+    "queue"("work_id");
 CREATE TABLE "storage"(
-    "storage_id" UUID NOT NULL,
+    "storage_id" SERIAL NOT NULL,
     "work_id" INTEGER NOT NULL,
     "location_type" VARCHAR(255) NOT NULL,
     "location_id" INTEGER NOT NULL,
     "uploaded_time" BIGINT NOT NULL,
     "updated_time" BIGINT NOT NULL,
     "location" BIGINT NOT NULL,
-    "patch_of" UUID NULL,
+    "patch_of" INTEGER NULL,
     "retrieved_from" VARCHAR(255) NOT NULL,
     "format" VARCHAR(255) NOT NULL
 );
