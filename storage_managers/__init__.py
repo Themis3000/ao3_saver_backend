5a420a05fc221c13306cc702d9b7a6f6725c2430
Themi Megas <Tcm4760@gmail.com>
2024-03-11 22:45:17 -0500

    added file compression

--- storage_managers/__init__.py
+++ storage_managers/__init__.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from db import get_head_work_storage_data, add_storage_entry, add_work_entry, update_storage_patch
 import uuid
 import bsdiff4
+import zlib
 
 
 class StorageManager(ABC):
@@ -17,21 +18,28 @@ class StorageManager(ABC):
     def get_file(self, key: str) -> bytes:
         pass
 
+    def store_file_compressed(self, key: str, data: bytes) -> None:
+        self.store_file(key, zlib.compress(data))
+
+    def get_file_compressed(self, key: str) -> bytes:
+        return zlib.decompress(self.get_file(key))
+
+# add zipping
     def store_work(self, work_id: int, work: bytes, uploaded_time: int, updated_time: int, retrieved_from: str,
                    file_format: str, work_title=None) -> None:
         storage_key = f"{work_id}_{uuid.uuid4()}"
-        self.store_file(storage_key, work)
+        self.store_file_compressed(storage_key, work)
         storage_id = add_storage_entry(work_id, uploaded_time, updated_time, storage_key, retrieved_from, file_format)
 
         head_work = get_head_work_storage_data(work_id, file_format)
         if head_work is not None:  # Create diff file to maintain history
-            old_work = self.get_file(head_work.location)
+            old_work = self.get_file_compressed(head_work.location)
             diff = bsdiff4.diff(work, old_work)
-            self.store_file(head_work.location, diff)
+            self.store_file_compressed(head_work.location, diff)
             update_storage_patch(head_work.storage_id, storage_id)
         else:  # Create record of work existing in db
             add_work_entry(work_id, True)
 
     def get_work(self, work_id: int, file_format: str) -> bytes:
         head_work = get_head_work_storage_data(work_id, file_format)
-        return self.get_file(head_work.location)
+        return self.get_file_compressed(head_work.location)
