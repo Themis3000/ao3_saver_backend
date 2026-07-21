2762a90b663c5a9a01da487479c09683bf2c26ce
Themi Megas <Tcm4760@gmail.com>
2026-07-21 04:38:27 -0500

    Addd requesting ip column

--- db_updater.py
+++ db_updater.py
@@ -1,4 +1,4 @@
-CURRENT_VERSION = 1
+CURRENT_VERSION = 2
 
 
 def get_db_version(conn):
@@ -30,44 +30,31 @@ def ensure_schema_updated(conn):
     print(f"got db version {version}")
 
     if version == CURRENT_VERSION:
+        print("database is up to date!")
         return
 
     init_cursor = conn.cursor()
     if version == 0:  # Migration script for version 0 -> 1
+        print("initializing database")
         with open("db_init.sql", "r") as f:
             init_cursor.execute(f.read())
-    # elif version == 1:  # Migration script for version 1 -> 2
-    #     init_cursor.execute("""
-    #         create table unfetched_objects
-    #         (
-    #             object_id       integer default nextval('object_index_object_id_seq'::regclass) not null
-    #                 constraint unfetched_objects_pk
-    #                     primary key,
-    #             request_url     varchar(2000)                                                   not null,
-    #             associated_work integer                                                         not null,
-    #             stalled         boolean default false                                           not null
-    #         );
-    #
-    #         create table object_dispatches
-    #         (
-    #             dispatch_id        serial                                       not null
-    #                 constraint object_dispatches_pk
-    #                     primary key,
-    #             dispatched_time    TIMESTAMP(0) WITHOUT TIME ZONE default NOW() not null,
-    #             dispatched_to_name varchar(255)                                 not null,
-    #             object_id          integer                                      not null,
-    #             fail_reported      boolean                        default false not null,
-    #             complete           boolean                        default false not null
-    #         );
-    #
-    #         create table version_info
-    #         (
-    #             version integer not null
-    #         );
-    #
-    #         INSERT INTO public.version_info (version)
-    #         VALUES (2);
-    #     """)
+    elif version == 1:  # Migration script for version 1 -> 2
+        print("upgrading database to version 2")
+        init_cursor.execute("""
+            create table version_info
+            (
+                version integer not null
+            );
+
+            INSERT INTO public.version_info (version)
+                VALUES (2);
+            
+            alter table dispatches
+                add requesting_ip varchar(255);
+
+            create index dispatches_request_ip
+                on dispatches (requesting_ip);
+        """)
 
     init_cursor.close()
     conn.commit()
